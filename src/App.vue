ea03165b366063f507c437e57bb311c233f0aa83
BeyondNaxos <adopteuntrip@gmail.com>
2023-07-31 17:53:36 +0200

    ref, reactive, emit

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,58 @@
 <template>
-    <HelloWorld />
+    <HelloWorld 
+    @update-name='name => display(name)'
+    />
+
+    <span>{{ sentence }}</span>
+
+    <br>
+    <br>
+
+
+    <!-- v-model permet d'associer une variable a l'input -->
+    <input type="text" v-model="name">
+    <input type="text" v-model="age">
 </template>
 
 
 <script setup>
+
 import HelloWorld from './components/HelloWorld.vue'
+// ref permet de faire reference à une variable  
+//  reactive permet de rendre une variable reactive c'est a dire qu'elle sera mise a jour dans le template
+import {watch, computed, ref} from 'vue'
+
+const display = ( name ) => {
+    console.log('app.vue : ' + name)
+}
+
+const sentence = computed(() => {
+return `Bonjour je m'appelle ${name.value} et j'ai ${age.value} ans`
+})
+
+
+const name = ref('John')
+const age = ref(29)
+
+watch(name, (newValue, oldValue) => {
+    console.log('name changed from ' + oldValue + ' to ' + newValue)
+})
+
+
 </script>
 
 
 
-<style scoped></style>
+<style scoped>
+.bold {
+    font-weight: bold;
+}
+
+.text-red {
+    color: indianred;
+}
+
+.text-green {
+    color: green;
+}
+</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,10 +1,28 @@
 <template>
     <h1>Hello world</h1>
+
+    {{ name }}
+
+    <input type="text" v-model="name" @input="handleUpate">
+    <slot name="name">
+
+    </slot>
+    <slot name="age">
+
+    </slot>
 </template>
     
     
 <script setup>
-    
+import { ref } from 'vue';
+
+const emit = defineEmits(['updateName'])
+
+const handleUpate = (event) => {
+emit('updateName', event.target.value)
+}
+
+const name = ref('' )
 </script>
     
     
